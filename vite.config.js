de7cefd4f086551423703f245bcec1fd7ba218aa
Vivek Chaudhary <vivekchaudhary.b@gmail.com>
2026-09-07 16:57:11 +0530

    live url change

--- vite.config.js
+++ vite.config.js
@@ -9,6 +9,7 @@ export default defineConfig({
     proxy: {
       '/api': {
         target: 'https://billing.hrmdpayrollsoftware.com',
+        // target: 'http://localhost:3001',
         changeOrigin: true,
       }
     }
